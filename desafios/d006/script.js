59330f990750526912c6f16c237f633c8d8c3cb4
octavioalicio <163463841+OAlicio@users.noreply.github.com>
2024-03-15 20:41:34 +0100

    Novo desafio

---
+++ desafios/d006/script.js
@@ -0,0 +1,13 @@
+let res = document.getElementById('res')
+function converter(){
+    let nc = window.prompt('Digite uma temperatura em graus celsius(ºC)')
+    if(nc.valueOf().length == 0){
+        window.alert('[ERRO] VALOR NULO OU INCORRECTO')
+        res.innerHTML += ``
+    }else{
+    res.innerHTML = ''    
+    let nf = ((Number(nc).valueOf()/5)*9)+32
+    let nk = Number(nc).valueOf() + 273
+    res.innerHTML += `<h2>A temperatura de ${nc}ºC, corresponde a...</h2><p>${nk}ºK (Kelvin) </p><p> ${nf}ºF (Fahrenheit) </p>`
+    }
+}
